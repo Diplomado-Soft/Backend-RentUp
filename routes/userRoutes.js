9a139760a9cb3f87f498f57e07e99adfa578ddfc
michael_papamija <astalejo6@gmail.com>
2026-04-12 10:15:26 -0500

    Google Firebase auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express'); // Framework para manejar rutas y peticiones HTTP
 const router = express.Router(); // Router para agrupar y manejar rutas
 const userController = require('../controllers/userController'); // Controlador para manejar la lógica de los arrendadores
+const authController = require('../controllers/authController'); // Controlador para auth Google Firebase
 const authMiddleware = require('../middlewares/authMiddleware'); // Middleware para manejar autenticación
 
 router.post('/signup', userController.signup);
@@ -10,6 +11,9 @@ router.put('/update', authMiddleware, userController.updateUserData);
 router.put('/update-whatsapp', authMiddleware, userController.updateWhatsApp);
 router.get('/getUser', authMiddleware, userController.getUserData);
 router.delete('/delete-account', authMiddleware, userController.deleteAccount);
+
+// Ruta Google Firebase Auth
+router.post('/auth/firebase-login', authController.firebaseLogin);
 
 // Exportación del router para usarlo en la aplicación principal
 module.exports = router;
